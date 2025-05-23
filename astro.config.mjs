9ff2e8776571fbfed7c365fc42d0c531d21a35d5
Spycner <pascal98kraus@gmail.com>
2025-05-23 22:48:59 +0200

    Enhance Astro config by disabling base styles in Tailwind integration and adding Vite CSS PostCSS configuration. Update CI workflow to include a build step for project compilation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   site: 'https://pkraus.github.io',
   base: '/website',
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  },
 });
