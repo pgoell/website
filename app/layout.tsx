8827389a7e29cfad7ea010c16cbc03f2809e1a0d
Spycner <pascal98kraus@gmail.com>
2026-02-01 11:38:38 +0100

    base next setup

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Pascal's Website",
+    description: "Blog and games"
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
