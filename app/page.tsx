23190af8f513f088f9450fafa2c63db364ebfbc2
Spycner <pascal98kraus@gmail.com>
2026-02-01 12:27:22 +0100

    feat: tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
+    <main className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Hello</h1>
     </main>
   );
 }
